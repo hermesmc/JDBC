5ded4afa42efb3b844ecfd603ad0f5c35fbfa56b
hermesmc <hermesmc@gmail.com>
2021-06-11 09:22:31 -0300

    Efetuando execlusoes

---
+++ src/TestaExclusao.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaExclusao {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao(); 
+		
+		Statement stm = connection.createStatement();
+		stm.execute("DELETE FROM produto WHERE (id = 6)");
+        
+		int contador = stm.getUpdateCount();
+		System.out.println("Linhas modificadas: " + contador);
+	}
+
+}
