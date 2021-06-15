d931272b176bc8b032d5b663432848d58af3dceb
hermesmc <hermesmc@gmail.com>
2021-06-15 09:03:37 -0300

    Utilizando try with resources

--- src/TestaInsercaoComparando.java
+++ src/TestaInsercaoComparando.java
@@ -10,26 +10,25 @@ public class TestaInsercaoComparando {
 		String nome = "mouse'";
 		String descricao = "mouse sem fio";
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.recuperarConexao(); 
+		try (Connection connection = connectionFactory.recuperarConexao()){ 
 
-		//Retira o controle de commit do JDBC
-		connection.setAutoCommit(false);
-
-		try {
-			PreparedStatement stm = connection.prepareStatement("INSERT INTO produto (id, nome, descricao) VALUES(0, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			//Retira o controle de commit do JDBC
+			connection.setAutoCommit(false);
 			
-			//adicionarVariavel(nome, descricao, stm);	
-			adicionarVariavel("SmartTV",  "45 polegadas", stm);
-			adicionarVariavel("Radio", "AM/FM stereo", stm);
-			
-			//Efetua o commit depois de todos os inserts 
-			connection.commit();		
-		} catch(Exception e){
-			e.printStackTrace();
-			System.out.println("Rollback executado.");
-			connection.rollback();
-		}	
-
+			// try with resources
+			try (PreparedStatement stm = connection.prepareStatement("INSERT INTO produto (id, nome, descricao) VALUES(0, ?, ?)", Statement.RETURN_GENERATED_KEYS);)
+			   {//adicionarVariavel(nome, descricao, stm);	
+				adicionarVariavel("SmartTV",  "45 polegadas", stm);
+				adicionarVariavel("Radio", "AM/FM stereo", stm);
+				
+				//Efetua o commit depois de todos os inserts 
+				connection.commit();		
+			} catch(Exception e){
+				e.printStackTrace();
+				System.out.println("Rollback executado.");
+				connection.rollback();
+			}	
+		}
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
@@ -41,12 +40,14 @@ public class TestaInsercaoComparando {
 		}
 		
 		stm.execute();
-		ResultSet resultado =  stm.getGeneratedKeys();
-		while(resultado.next()) {
-			Integer id = resultado.getInt(1);
-			System.out.println("Foi criado o ID:" + id);
-		}    
 		
-		resultado.close();
+		// try with resources
+		try (ResultSet resultado =  stm.getGeneratedKeys()){
+			while(resultado.next()) {
+				Integer id = resultado.getInt(1);
+				System.out.println("Foi criado o ID:" + id);
+			}
+		}		
+		
 	}
 }
